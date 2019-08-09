a53cb5c236f5bf6ba20cda799779ad847eb93f9b
user2 <user2@KH_C>
2019-08-09 21:18:46 +0900

    테스트입니다.

--- src/com/kh/gitHubTest/MasterCode.java
+++ src/com/kh/gitHubTest/MasterCode.java
@@ -3,7 +3,8 @@ package com.kh.gitHubTest;
 public class MasterCode {
 
 	public static void main(String[] args) {
-		System.out.println("Hi 나는 서범수입니다.");
+		System.out.println("Hi 나는 이범희입니다.");
+		System.out.println("테스트입니다");
 
 	}
 
